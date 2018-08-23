LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: Set vendor props via PRODUCT_PROPERTY_OVERRIDES
 * Support for vendor.prop was abandoned

Change-Id: I3554bfc57c27555f8c605e8555ea5f386bce0c5d

vendor_prop.mk
@@ -1,0 +1,165 @@
+#
+# Common vendor properties for msm8996
+#
+
+# Adreno
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qcom.adreno.qgl.ShaderStorageImageExtendedFormats=0
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=1 \
+    audio.deep_buffer.media=true \
+    audio.offload.min.duration.secs=15 \
+    audio.offload.video=true \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.bt.enable.splita2dp=false \
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=7 \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    ro.vendor.audio.sdk.ssr=false \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio_hal.period_size=192 \
+    vendor.audio.hw.aac.encoder=true \
+    vendor.audio.offload.buffer.size.kb=64 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.tunnel.encode=false \
+    vendor.voice.path.for.pcm.voip=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    bt.max.hfpclient.connections=1 \
+    qcom.bluetooth.soc=rome \
+    ro.bluetooth.dun=true \
+    ro.bluetooth.hfp.ver=1.7 \
+    ro.bluetooth.sap=true \
+    ro.btconfig.if=uart \
+    ro.btconfig.vendor=qcom \
+    ro.btconfig.chip=QCA6164 \
+    ro.btconfig.dev=/dev/ttyHS0
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.gyro.disable=0 \
+    persist.camera.imglib.fddsp=1
+
+# CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.cne.feature=1
+
+# Data modules
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.data.mode=concurrent \
+    persist.data.netmgrd.qos.enable=true \
+    ro.use_data_netmgrd=true
+
+# Display (Qualcomm AD)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.cabl=0 \
+    ro.qcom.ad=1 \
+    ro.qcom.ad.calib.data=/system/etc/calib.cfg \
+    ro.qcom.ad.sensortype=2
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.egl.hw=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    debug.sf.hw=1 \
+    debug.sf.latch_unsignaled=1 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.demo.hdmirotationlock=false \
+    persist.hwc.enable_vds=1 \
+    persist.sys.wfd.virtual=0 \
+    ro.opengles.version=196610 \
+    ro.sf.lcd_density=480 \
+    sdm.debug.disable_rotator_split=1 \
+    sdm.debug.disable_skip_validate=1 \
+    sdm.perf_hint_window=50
+
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.vidc.enc.disable.pq=true \
+    vidc.enc.dcvs.extra-buff-count=2
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.nfc.port=I2C \
+    persist.nfc.smartcard.config=SIM1,SIM2,eSE1
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.qti.sys.fw.bg_apps_limit=60
+
+# Priv-app permissions whitelist
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.control_privapp_permissions=enforce
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ril.subscription.types=NV,RUIM \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.default_cdma_sub=0 \
+    ro.telephony.default_network=20,20 \
+    persist.data.qmi.adb_logmask=0 \
+    persist.net.doxlat=true \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.force_on_dc=true \
+    persist.radio.multisim.config=dsds \
+    persist.radio.redir_party_num=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.sib16_support=1
+
+# RmNet Data
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rmnet.data.enable=true \
+    persist.data.wda.enable=true \
+    persist.data.df.dl_mode=5 \
+    persist.data.df.ul_mode=5 \
+    persist.data.df.agg.dl_pkt=10 \
+    persist.data.df.agg.dl_size=4096 \
+    persist.data.df.mux_count=8 \
+    persist.data.df.iwlan_mux=9 \
+    persist.data.df.dev_name=rmnet_usb0
+
+# Security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2018-06-01
+
+# TimeService
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.usb.controller=6a00000.dwc3
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0

Dyneteve/android_device_zuk_msm8996-common

vendor_prop.mk
@@ -1,0 +1,165 @@
+#
+# Common vendor properties for msm8996
+#
+
+# Adreno
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qcom.adreno.qgl.ShaderStorageImageExtendedFormats=0
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=1 \
+    audio.deep_buffer.media=true \
+    audio.offload.min.duration.secs=15 \
+    audio.offload.video=true \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.bt.enable.splita2dp=false \
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=7 \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    ro.vendor.audio.sdk.ssr=false \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio_hal.period_size=192 \
+    vendor.audio.hw.aac.encoder=true \
+    vendor.audio.offload.buffer.size.kb=64 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.tunnel.encode=false \
+    vendor.voice.path.for.pcm.voip=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    bt.max.hfpclient.connections=1 \
+    qcom.bluetooth.soc=rome \
+    ro.bluetooth.dun=true \
+    ro.bluetooth.hfp.ver=1.7 \
+    ro.bluetooth.sap=true \
+    ro.btconfig.if=uart \
+    ro.btconfig.vendor=qcom \
+    ro.btconfig.chip=QCA6164 \
+    ro.btconfig.dev=/dev/ttyHS0
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.preview.ubwc=0 \
+    persist.camera.video.ubwc=0 \
+    persist.camera.gyro.disable=0 \
+    ro.persist.qcapb=1 \
+    camera.disable_zsl_mode=1 \
+
+# CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.cne.feature=1
+
+# Data modules
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.data.mode=concurrent \
+    persist.data.netmgrd.qos.enable=true \
+    ro.use_data_netmgrd=true
+
+# Display (Qualcomm AD)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.cabl=0 \
+    ro.qcom.ad=1 \
+    ro.qcom.ad.calib.data=/vendor/etc/calib.cfg \
+    ro.qcom.ad.sensortype=2
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.egl.hw=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    debug.sf.hw=1 \
+    debug.sf.latch_unsignaled=1 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.demo.hdmirotationlock=false \
+    persist.hwc.enable_vds=1 \
+    persist.sys.wfd.virtual=0 \
+    ro.opengles.version=196610 \
+    ro.sf.lcd_density=480 \
+    sdm.debug.disable_rotator_split=1 \
+    sdm.debug.disable_skip_validate=1 \
+    sdm.perf_hint_window=50
+
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.vidc.enc.disable.pq=true \
+    vidc.enc.dcvs.extra-buff-count=2
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.qti.sys.fw.bg_apps_limit=60
+
+# Priv-app permissions whitelist
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.control_privapp_permissions=enforce
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ril.subscription.types=NV,RUIM \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.default_cdma_sub=0 \
+    ro.telephony.default_network=20,20 \
+    persist.data.qmi.adb_logmask=0 \
+    persist.net.doxlat=true \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.force_on_dc=true \
+    persist.radio.multisim.config=dsds \
+    persist.radio.redir_party_num=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.sib16_support=1
+
+# RmNet Data
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rmnet.data.enable=true \
+    persist.data.wda.enable=true \
+    persist.data.df.dl_mode=5 \
+    persist.data.df.ul_mode=5 \
+    persist.data.df.agg.dl_pkt=10 \
+    persist.data.df.agg.dl_size=4096 \
+    persist.data.df.mux_count=8 \
+    persist.data.df.iwlan_mux=9 \
+    persist.data.df.dev_name=rmnet_usb0
+
+# Security patch level
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.vendor.build.security_patch=2018-06-01
+
+# TimeService
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true \
+    persist.delta_time.enable=true
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.usb.controller=6a00000.dwc3 \
+    persist.sys.usb.config.extra=none
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0